10ed886734fbbfe9573c9be1bef92afb9197e4dd
yunyunss <74299519+yunyunss@users.noreply.github.com>
2023-10-18 00:18:26 +0900

    .

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-a = [1, 2, 3, 4, 4]
-print(a.index(4))
+# import random
+
+# a = []
+# r = []
+
+# while len(r) != 6:
+#     c = random.randint(1, 6)
+#     if c in a:
+#         continue
+#     r.append(c)
+#     a.append(c)
+    
+# print(r)
+
+import pyautogui, time
+
+while True:
+    pyautogui.click()
+    time.sleep(0.1)
